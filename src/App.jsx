658f739a04982c7d030f5131dc238ae09d3e99f2
Geice Gomes <geice.gomes@gruponewway.com.br>
2022-08-02 10:29:16 -0300

    netflix react

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+import categories from "./Api";
+import Row from "./components/Row/index";
+import Banner from "./components/Banner/index";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Nacbar */}
+      <Banner />
+      {/* Em Alta */}
+      {categories.map((category) => {
+        return (
+          <Row
+            key={category.nome}
+            title={category.title}
+            path={category.path}
+            isLarge={category.isLarge}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
