c0d331ef7cce9e6007772586dadd1b636a1cf99c
Geice Gomes <sogmesmo@gmail.com>
2022-08-02 11:56:56 -0300

    component banner

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import "./App.css";
 import categories from "./Api";
 import Row from "./components/Row/index";
 import Banner from "./components/Banner/index";
+import Nav from "./components/Nav/index";
 
 function App() {
   return (
     <div className="App">
-      {/* Nacbar */}
+      <Nav />
       <Banner />
       {/* Em Alta */}
       {categories.map((category) => {
